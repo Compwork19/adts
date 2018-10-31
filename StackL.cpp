dd1f7884c123b2a4e94205e86c60cf604bb7816a
Compwork19 <Compwork19@users.noreply.github.com>
2018-10-31 16:02:05 -0400

    Added stackl file

--- StackL.cpp
+++ StackL.cpp
@@ -1,1 +1,34 @@
 #include "StackL.h"
+#include<iostream>
+
+
+int Stack::size()
+{
+	data.size();
+}
+
+void Stack::push(int b)
+{
+	   data.insert(b,1);
+   }
+   
+void Stack::pop()
+{
+	   data.remove(1);
+   }
+
+int Stack::top()
+{
+	  if(data.size()==0)
+	  cout << "The stack is empty " << endl;
+	  else
+	  return data.get(1); 
+	   
+   }
+
+void Stack::clear()
+{
+	   while(size()!=0);
+	   data.remove(1);
+	   
+   }
