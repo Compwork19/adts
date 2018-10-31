7edea7cc571f998d72963058e41066017272d4f7
Compwork19 <Compwork19@users.noreply.github.com>
2018-10-31 16:27:01 -0400

    Added stackll file

--- StackLL.cpp
+++ StackLL.cpp
@@ -1,1 +1,52 @@
 #include "StackLL.h"
+#include<iostream>
+
+class Stack::Node 
+{
+	public:
+	   int data = 0;
+	   Node* link = nullptr;
+};
+
+Stack::~Stack()
+{
+	while(size()!=0)
+	pop();
+}
+
+int Stack::size()
+{
+	return num_elements;
+}
+
+void Stack::push(int val)
+{
+	Node* newPtr = new Node{val};
+	newPtr->link = frontPtr;
+    frontPtr = newPtr;
+    num_elements++;
+    
+ }
+
+void Stack::pop()
+{
+	Node* delPtr;
+	delPtr = frontPtr;
+    frontPtr = frontPtr->link;
+    delete delPtr;
+    num_elements--;
+}
+
+int Stack::top()
+{
+	if(size()==0)
+	  return 00;
+	else
+	  return frontPtr->data; 
+}
+
+void Stack::clear()
+{
+	while(size()!=0)
+	 pop();
+}
